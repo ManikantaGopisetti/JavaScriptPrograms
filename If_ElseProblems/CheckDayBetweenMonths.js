2d43e704fc5a715a968232033137ce1f27c53389
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-12-29 01:04:32 +0530

    Refractor:correct indentation for Selection Practice Problems with if & else

--- If_ElseProblems/CheckDayBetweenMonths.js
+++ If_ElseProblems/CheckDayBetweenMonths.js
@@ -1,17 +1,22 @@
-let day=19;
-let month=6;
+let day=31;
+let month=5;
 
 //checking if the month is between march and june
-if(month>=3&&month<=6){
-    //checking to see if day is after march 20 && before june
-    if(day>20&&month<6){
-        console.log("True");
-    }
+if(month>=3 && month<=6){
+    //check the days in particular month
+    if(day>0 && day<=30+(month%2)){
+        //checking to see if day is after march 20 && before june
+        if(day>20&&month<6){
+            console.log("True");
+        }
     //checking to see if day is after march and before june 20
-    else if(day<20&&month>3){
-        console.log("True");
+        else if(day<20 && month>3){
+         console.log("True");
+        }else{
+          console.log("False");
+        }
     }else{
-        console.log("False");
+        console.log("Enter a valid day");
     }
 }else{
     console.log("False");
